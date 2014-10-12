8ca79129848930db23ad148b712f8d6d71512ae6
Valeriano A.R <valen.alfonso@gmail.com>
2014-10-13 01:55:16 +0200

    Commit inicial

---
+++ FilenameNormalizer/FrmFilenameNormalizer.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace FilenameNormalizer
+{
+    partial class FrmFilenameNormalizer
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.lsbFiles = new System.Windows.Forms.ListBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtSuffix = new System.Windows.Forms.TextBox();
+            this.cbFilter = new System.Windows.Forms.ComboBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.lblSuffix = new System.Windows.Forms.Label();
+            this.lblFilter = new System.Windows.Forms.Label();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(79, 6);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(232, 20);
+            this.txtPath.TabIndex = 0;
+            // 
+            // lsbFiles
+            // 
+            this.lsbFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lsbFiles.FormattingEnabled = true;
+            this.lsbFiles.Location = new System.Drawing.Point(15, 118);
+            this.lsbFiles.Name = "lsbFiles";
+            this.lsbFiles.Size = new System.Drawing.Size(296, 160);
+            this.lsbFiles.TabIndex = 1;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGo.Location = new System.Drawing.Point(263, 284);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(48, 32);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtSuffix
+            // 
+            this.txtSuffix.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSuffix.Location = new System.Drawing.Point(79, 32);
+            this.txtSuffix.Name = "txtSuffix";
+            this.txtSuffix.Size = new System.Drawing.Size(232, 20);
+            this.txtSuffix.TabIndex = 3;
+            // 
+            // cbFilter
+            // 
+            this.cbFilter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFilter.FormattingEnabled = true;
+            this.cbFilter.Location = new System.Drawing.Point(79, 58);
+            this.cbFilter.Name = "cbFilter";
+            this.cbFilter.Size = new System.Drawing.Size(232, 21);
+            this.cbFilter.TabIndex = 4;
+            this.cbFilter.SelectedIndexChanged += new System.EventHandler(this.cbFilter_SelectedIndexChanged);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTest.Location = new System.Drawing.Point(213, 290);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(44, 23);
+            this.btnTest.TabIndex = 5;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(12, 9);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(30, 13);
+            this.lblPath.TabIndex = 6;
+            this.lblPath.Text = "Ruta";
+            // 
+            // lblSuffix
+            // 
+            this.lblSuffix.AutoSize = true;
+            this.lblSuffix.Location = new System.Drawing.Point(12, 35);
+            this.lblSuffix.Name = "lblSuffix";
+            this.lblSuffix.Size = new System.Drawing.Size(33, 13);
+            this.lblSuffix.TabIndex = 7;
+            this.lblSuffix.Text = "Sufijo";
+            // 
+            // lblFilter
+            // 
+            this.lblFilter.AutoSize = true;
+            this.lblFilter.Location = new System.Drawing.Point(12, 61);
+            this.lblFilter.Name = "lblFilter";
+            this.lblFilter.Size = new System.Drawing.Size(29, 13);
+            this.lblFilter.TabIndex = 8;
+            this.lblFilter.Text = "Filtro";
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilter.Location = new System.Drawing.Point(79, 86);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(232, 20);
+            this.txtFilter.TabIndex = 9;
+            // 
+            // FrmFilenameNormalizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 327);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.lblFilter);
+            this.Controls.Add(this.lblSuffix);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.cbFilter);
+            this.Controls.Add(this.txtSuffix);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lsbFiles);
+            this.Controls.Add(this.txtPath);
+            this.MinimumSize = new System.Drawing.Size(339, 366);
+            this.Name = "FrmFilenameNormalizer";
+            this.Text = "FilenameNormalizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.ListBox lsbFiles;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtSuffix;
+        private System.Windows.Forms.ComboBox cbFilter;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Label lblSuffix;
+        private System.Windows.Forms.Label lblFilter;
+        private System.Windows.Forms.TextBox txtFilter;
+    }
+}
+
---
+++ FilenameNormalizer/FrmFilenameNormalizer.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FilenameNormalizer
+{
+    public partial class FrmFilenameNormalizer : Form
+    {
+        public string InitialPath
+        {
+            get { return txtPath.Text; }
+            set { txtPath.Text = value; }
+        }
+
+        public FrmFilenameNormalizer()
+        {
+            InitializeComponent();
+            txtPath.Text = Directory.GetCurrentDirectory();
+            cbFilter_Init();
+        }
+
+        #region Filter
+
+        private void cbFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtFilter.Enabled = (cbFilter.SelectedIndex == 4);
+        }
+
+        private void cbFilter_Init()
+        {
+            this.cbFilter.Items.AddRange(new object[] {
+                "All",
+                "DSC_ + MOV_",
+                "IMG_ + VID_",
+                "DSC_ + MOV_ + IMG_ + VID_",
+                "Custom"});
+            cbFilter.SelectedIndex = 0;
+            txtFilter.Enabled = false;
+        }
+
+        private bool cbFilter_ApplyFilter(string fileName)
+        {
+            switch(cbFilter.SelectedIndex){
+                case 0:
+                    return true;
+                case 1:
+                    return (fileName.StartsWith("DSC_") || fileName.StartsWith("MOV_"));
+                case 2:
+                    return (fileName.StartsWith("IMG_") || fileName.StartsWith("VID_"));
+                case 3:
+                    return (fileName.StartsWith("DSC_") || fileName.StartsWith("IMG_") || fileName.StartsWith("MOV_") || fileName.StartsWith("VID_"));
+                case 4:
+                    return txtFilter_ApplyFilter(fileName);
+            }
+            return false;
+        }
+
+        private bool txtFilter_ApplyFilter(string fileName)
+        {
+            if (string.IsNullOrEmpty(txtFilter.Text))
+            {
+                return false;
+            }
+
+            string[] filters = txtFilter.Text.Split('|');
+
+            foreach (string strFilter in filters)
+            {
+                if (fileName.Contains(strFilter))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        #endregion
+
+        private void lsbFiles_Clean()
+        {
+            lsbFiles.Items.Clear();
+        }
+
+        private void lsbFiles_AddLine(string line)
+        {
+            lsbFiles.Items.Add(line);
+
+            Application.DoEvents();
+
+            int visibleItems = lsbFiles.ClientSize.Height / lsbFiles.ItemHeight;
+            lsbFiles.TopIndex = Math.Max(lsbFiles.Items.Count - visibleItems + 1, 0);
+        }
+
+        /// <summary>
+        /// Returns the EXIF Image Data of the Date Taken.
+        /// </summary>
+        /// <param name="getImage">Image (If based on a file use Image.FromFile(f);)</param>
+        /// <returns>Date Taken or Null if Unavailable</returns>
+        public static DateTime? DateTaken(Image getImage)
+        {
+            int DateTakenValue = 0x9003; //36867;
+
+            if (!getImage.PropertyIdList.Contains(DateTakenValue))
+                return null;
+
+            string dateTakenTag = System.Text.Encoding.ASCII.GetString(getImage.GetPropertyItem(DateTakenValue).Value);
+            string[] parts = dateTakenTag.Split(':', ' ');
+            int year = int.Parse(parts[0]);
+            int month = int.Parse(parts[1]);
+            int day = int.Parse(parts[2]);
+            int hour = int.Parse(parts[3]);
+            int minute = int.Parse(parts[4]);
+            int second = int.Parse(parts[5]);
+
+            return new DateTime(year, month, day, hour, minute, second);
+        }
+
+        public static DateTime FirstDate(string filePath)
+        {
+            DateTime dtFile;
+
+            string ext = Path.GetExtension(filePath).ToLower();
+
+            DateTime dtCretation = File.GetCreationTime(filePath);
+            DateTime dtLastMod = File.GetLastWriteTime(filePath);
+            if (dtCretation < dtLastMod)
+            {
+                dtFile = dtCretation;
+            }
+            else
+            {
+                dtFile = dtLastMod;
+            }
+
+            if (ext == ".png" || ext == ".jpg" || ext == ".jpeg" || ext == ".gif")
+            {
+                Image img = Image.FromFile(filePath);
+                DateTime? dtTaken = DateTaken(img);
+                if (dtTaken != null)
+                {
+                    dtFile = (DateTime)dtTaken;
+                }
+                img.Dispose();
+            }
+
+            return dtFile;
+        }
+
+        private void ProcessDirectory(string path, bool doMove)
+        {
+            string[] filePaths = Directory.GetFiles(path);
+            foreach (string filePath in filePaths)
+            {
+                string fileName = Path.GetFileName(filePath);
+
+                if (cbFilter_ApplyFilter(fileName))
+                {
+                    DateTime dtFile = FirstDate(filePath);
+                    string ext = Path.GetExtension(filePath).ToLower();
+                    string fileDirPath = Path.GetDirectoryName(filePath);
+                    string newFilePath = string.Format("{0}/{1}{2}{3}", fileDirPath, dtFile.ToString("yyyyMMdd-HHmmss"), txtSuffix.Text, ext);
+
+
+                    if (doMove)
+                    {
+                        int retry = 1;
+                        while (File.Exists(newFilePath))
+                        {
+                            newFilePath = string.Format("{0}/{1}-{2}{3}{4}", fileDirPath, dtFile.ToString("yyyyMMdd-HHmmss"), retry, txtSuffix.Text, ext);
+                            retry++;
+                        }
+                        File.Move(filePath, newFilePath);
+                    }
+
+                    lsbFiles_AddLine(string.Format("RenameTo: {0} {1} ", fileName, newFilePath));
+                }
+                else
+                {
+                    lsbFiles_AddLine(string.Format("Unchanged: {0}", fileName));
+                }
+            }
+
+            string[] directoryPaths = Directory.GetDirectories(path);
+            foreach (string directoryPath in directoryPaths)
+            {
+                ProcessDirectory(directoryPath, doMove);
+            }
+        }
+
+        private void Process(bool doMove)
+        {
+            string path = txtPath.Text;
+            if (!Directory.Exists(path))
+            {
+                MessageBox.Show("El directorio no existe");
+                return;
+            }
+
+            lsbFiles_Clean();
+            ProcessDirectory(path, doMove);
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            Process(true);
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            Process(false);
+        }
+
+    }
+}
